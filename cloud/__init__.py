1a876ad20b0b0407a62af73c8f32d5da8b923d64
ZZanetti <zzanetti1993@gmail.com>
2020-07-14 12:06:02 -0400

    added cloud.py and simple function to make a html word cloud

---
+++ cloud/__init__.py
@@ -0,0 +1,21 @@
+def makeCloudtext(words, counts):
+ assert(len(words) == len(counts)), "input arrays must be of equal length"
+ max_index = len(words)
+ output_array = []
+ word = ""
+
+ for i in range(0,max_index):
+     word = words[i]
+     tag = f"<h2>{word}</h2>"
+     if counts[i] > 1:
+         output_array.append(tag)
+ return output_array    
+
+def makeCloud(text_array):
+    try:
+        f = open("demo.html", "w")
+        f.write(" ".join(text_array))
+    except:
+        print("failed")
+    finally:
+        f.close()
--- test.py
+++ test.py
@@ -1,13 +1,37 @@
 import unittest
 import my_word
+import cloud
 
 sanitycheck = my_word.sanitycheck
 polishWord = my_word.polishWord
+makeCloudtext = cloud.makeCloudtext
 
 class TestSum(unittest.TestCase):
 
     def test_wordReplace(self):
         self.assertEqual(polishWord("terror", "fear"), "fear", "Should be fear")
+
+        # now test polish word with different sets of words 
+
+    def test_makeCloudtext(self):
+        self.assertEqual(makeCloudtext(["fear", "love"], [1, 4]), ["<h2>love</h2>"], "Should be equal")    
+
+    #test for replacing words in a sectence based on one target word
+    #test with target and key sentences
+    #test with syno cousins, i.e. not direct synonyms but share over 70% 
+
+
+    #quantification tests
+    #parser tests, .txt file with hello world, word count, other data,
+    #test overlap, "fear", "fear" = 1, how many overlaps?
+    #word count (should return key: 1 target: 1 )
+    #overlap percentage (100%)
+
+
+    #display histogram of job posting words 
+
+    # make wordcloud <tag>word </tag>, tag based on frequency, output to .html
+
 
 if __name__ == '__main__':
     unittest.main()
